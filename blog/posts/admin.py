a9d6d67ff7d1d0032111803943583b6511a5d16c
kropiak <kropiak@matman.uwm.edu.pl>
2025-11-16 18:27:46 +0100

    koniec lab 3

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Topic
 
-# Register your models here.
+# a następnie zarejestrować (pokazano najprostszy przypadek)
+admin.site.register(Category)
+admin.site.register(Topic)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(null=True, blank=True)
+    # description2 = models.TextField()
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=60)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
