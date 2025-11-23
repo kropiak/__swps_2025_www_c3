105d3c0f86f0e6bfb578a322ebc58e3da0575dec
kropiak <kropiak@matman.uwm.edu.pl>
2025-11-23 10:26:31 +0100

    dodanie klasy PostAdmin

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from .models import Category, Topic
+from .models import Category, Topic, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ['title', 'text', 'topic', 'slug', 'created_by', 'created_at', 'updated_at']
+    readonly_fields = ['created_by', 'created_at', 'updated_at']
+    list_display = ['title', 'short_text', 'topic']
+
+    @admin.display(description='Short text')
+    def short_text(self, obj):
+        words = f'{obj.text}'.split()
+        if len(words) <= 5:
+            return ' '.join(words)
+        else:
+            return ' '.join(words[:5]) + ' ...'
+
+
 
 # a następnie zarejestrować (pokazano najprostszy przypadek)
 admin.site.register(Category)
 admin.site.register(Topic)
+admin.site.register(Post, PostAdmin)
+
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import title
 
 
 class Category(models.Model):
@@ -8,9 +10,28 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=60)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Post(models.Model):
+    title =  models.CharField(max_length=150)
+    text =  models.TextField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.title}'
 
